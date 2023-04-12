2a0f9fc8a29bb5f305e756acef021de93be4a8e6
Niyas Kunnakkadan <niyaskunnekkadan@gmail.com>
2023-04-12 12:44:22 +0530

    progress one

---
+++ 07_progress_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const ScreenHome(),
+    );
+  }
+}
+
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CircPercent(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CircPercent extends StatelessWidget {
+  const CircPercent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularPercentIndicator(
+      radius: 100,
+      lineWidth: 20,
+      startAngle: 10,
+      percent: .6,
+      backgroundColor: Colors.purple.shade100,
+      progressColor: Colors.purple,
+      backgroundWidth: 18,
+      center: const Text('60% Processing...'),
+      animation: true,
+      animationDuration: 1800,
+      restartAnimation: true,
+    );
+  }
+}
+
